18c4d8d6e491051a5ddf5b46ac851c07f88fc457
shiramashiro <zhengrenfu@outlook.com>
2022-08-14 17:42:28 +0800

    调整代码缩进

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -3,13 +3,8 @@ const path = require("path");
 module.exports = {
   entry: "./src/main.ts",
   mode: "production",
-  output: {
-    path: path.resolve(__dirname, "dist/pro"),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [ ".ts", ".tsx", ".js" ]
-  },
+  output: { path: path.resolve(__dirname, "dist/pro"), filename: "bundle.js" },
+  resolve: { extensions: [ ".ts", ".tsx", ".js" ] },
   module: {
     rules: [
       { test: /\.tsx?$/, loader: "ts-loader", exclude: /(node_modules|bower_components)/ },
