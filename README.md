b757369347e4c4d6c6df216b3464e755131c361d
shiramashiro <zhengrenfu@outlook.com>
2022-08-14 17:37:13 +0800

    version 1 版本的模板，将 babel 配置抽离出单独的配置文件中

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
+# 安装说明
+
 下载到本地之后，终端打开项目根目录，安装依赖：
 
 ```shell
 npm install
 ```
 
+# 项目介绍
+
 该项目是 Webpack + TypeScript + Babel 的模板，主入口文件是 `./src/main.ts`。
 
-使用了 ts-loader、babel-loader。babel 的预设使用了 @babel/preset-typescript、@babel/preset-env，并使用了插件 @babel/preset-env。
+## 依赖介绍
 
-初始的 package.json 脚本有 dev 和 build。build 少了 sourcemaps 功能，dev 可以使用 sourcemaps 功能，方便控制台调试。
+加载器有：ts-loader、babel-loader、source-map-loader。
+
+babel 的预设器有：@babel/preset-typescript、@babel/preset-env。
+
+package.json 脚本有 dev 和 build。build 少了 sourcemaps 功能，dev 可以使用 sourcemaps 功能，方便控制台调试。
+
+# version1 分支
+
+这个分支的 babel 配置选项不再 webpack.config.*.js 中，而是单独存在于 babel.config.json 文件。
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,39 +3,14 @@ const path = require("path");
 module.exports = {
   entry: "./src/main.ts",
   mode: "development",
-  output: {
-    path: path.resolve(__dirname, "dist/dev"),
-    filename: "bundle.js"
-  },
+  output: { path: path.resolve(__dirname, "dist/dev"), filename: "bundle.js" },
   devtool: "source-map",
-  resolve: {
-    extensions: [ ".ts", ".tsx", ".js" ]
-  },
-module: {
-  rules: [
-    { test: /\.tsx?$/, loader: "ts-loader", exclude: /(node_modules|bower_components)/ },
-    {
-      test: /\.ts$/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: {
-                  "chrome": "58",
-                  "ie": "11"
-                }
-              }
-            ], [ "@babel/preset-typescript" ]
-          ],
-          plugins: [ "@babel/transform-runtime" ]
-        }
-      },
-      exclude: /(node_modules|bower_components)/
-    },
-    { test: /\.js$/, loader: "source-map-loader", exclude: /(node_modules|bower_components)/ }
-  ]
-}
+  resolve: { extensions: [ ".ts", ".tsx", ".js" ] },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader", exclude: /(node_modules|bower_components)/ },
+      { test: /\.ts$/, loader: "babel-loader", exclude: /(node_modules|bower_components)/ },
+      { test: /\.js$/, loader: "source-map-loader", exclude: /(node_modules|bower_components)/ }
+    ]
+  }
 };
